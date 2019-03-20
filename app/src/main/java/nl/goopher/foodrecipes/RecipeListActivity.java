d2c7e527d67034d345fc3037dd357419a1722267
Goopher Maijenburg <goopher.maijenburg@soprabanking.com>
2019-03-20 15:00:35 +0100

    Before viewmodel creation

--- app/src/main/java/nl/goopher/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/nl/goopher/foodrecipes/RecipeListActivity.java
@@ -12,11 +12,13 @@ import java.util.List;
 import nl.goopher.foodrecipes.models.Recipe;
 import nl.goopher.foodrecipes.requests.RecipeApi;
 import nl.goopher.foodrecipes.requests.ServiceGenerator;
+import nl.goopher.foodrecipes.requests.responses.RecipeResponse;
 import nl.goopher.foodrecipes.requests.responses.RecipeSearchResponse;
 import nl.goopher.foodrecipes.util.Constants;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
 
 public class RecipeListActivity extends BaseActivity {
     private static final String TAG = "RecipeListActivity";
@@ -38,22 +40,18 @@ public class RecipeListActivity extends BaseActivity {
     private void testRetrofitRequest() {
         RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
 
-        Call<RecipeSearchResponse> responseCall = recipeApi
-                .searchRecipe(Constants.API_KEY,
-                        "chicken breast",
-                        "1");
+        Call<RecipeResponse> responseCall = recipeApi
+                .getRecipe(Constants.API_KEY,
+                        "495802");
 
-        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+        responseCall.enqueue(new Callback<RecipeResponse>() {
             @Override
-            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
                 Log.d(TAG, "onResponse: server response: " + response.toString());
-                if(response.code() == 200) {
+                if (response.code() == 200) {
                     Log.d(TAG, "onResponse: " + response.body().toString());
-                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
-                    for (Recipe recipe :
-                            recipes) {
-                        Log.d(TAG, "onResponse: " + recipe.getTitle());
-                    }
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: RETRIEVED RECIPE: " + recipe);
                 } else {
                     try {
                         Log.d(TAG, "onResponse: " + response.errorBody().string());
@@ -63,9 +61,10 @@ public class RecipeListActivity extends BaseActivity {
                 }
             }
 
+
             @Override
-            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
-                Log.e(TAG, "onFailure: ",t);
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
+
             }
         });
     }
